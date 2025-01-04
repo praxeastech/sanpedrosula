bd4cdfa28ae9af0aba6494135f77868172b3d746
David Blanchard <dblanchard@praxeas.com>
2025-01-03 20:35:28 -0600

    Add index

--- assets/docs/Curriculum/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dir = opendir("./Espanol/");
-while ($file = readdir($dir)) {
-    if ($file != "." && $file != "..") {
-        echo "<a href='$file'>$file</a><br>";
-    }
-}
-closedir($dir);
-?>
